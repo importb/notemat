c95d8c59ed5c4c73201a19619fdf7c6fba5f9699
importb <rainervana1@gmail.com>
2025-03-17 16:14:44 +0200

    full javafx rewrite.

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-MODULE_PATH="`find ./ -name javafx-base-20.0.2-linux.jar`:`find ./ -name javafx-controls-20.0.2-linux.jar`:`find ./ -name javafx-fxml-20.0.2-linux.jar`:`find ./ -name javafx-graphics-20.0.2-linux.jar`:`find ./ -name javafx-swing-20.0.2-linux.jar`"
-
-java --module-path "$MODULE_PATH" --add-modules javafx.controls,javafx.fxml,javafx.graphics,javafx.base,javafx.swing -jar notematjavafx-1.0-SNAPSHOT.jar
