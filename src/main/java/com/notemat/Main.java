69277241479840aa45396903ba6207e7ae0729c9
importb <rainervana1@gmail.com>
2025-03-19 10:11:23 +0200

    Text smoothing.

--- src/main/java/com/notemat/Main.java
+++ src/main/java/com/notemat/Main.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class Main extends Application {
     public static void main(String[] args) {
+        System.setProperty("prism.lcdtext", "false");
         launch(args);
     }
 
